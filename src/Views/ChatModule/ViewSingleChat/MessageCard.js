09f8170484a316e13221b7122336f01f0f60cf45
Binara Medawatta <binaramedawatta@gmail.com>
2020-06-01 11:46:34 +0530

    View Single message component

---
+++ src/Views/ChatModule/ViewSingleChat/MessageCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function MessageCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {props.message.name}
+        </Typography>
+        <Typography variant="body1" component="h2">
+          {props.message.message}
+        </Typography>
+        <Typography variant="body2" component="p" color="textSecondary">
+            {props.message.time}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
