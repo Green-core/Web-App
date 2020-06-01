a88a61d83998351e29aa07f533a69540b0dfc3ac
Binara Medawatta <binaramedawatta@gmail.com>
2020-06-01 11:47:42 +0530

    View all chats component

--- src/Template/Content/Content.js
+++ src/Template/Content/Content.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Auth from '../../Layouts/Auth'
+import ViewSingleMessage from '../../Views/ChatModule/ViewSingleChat/ViewSingleMessage'
+import ViewAllChats from '../../Views/ChatModule/ViewAllChats/ViewAllChats'
 
 function Content(){
     return (
-        <div>
-            H!
-        </div>
+        <ViewSingleMessage />
+        // <ViewAllChats />
+        // <Switch>
+        //     <Route path="/auth">
+        //         <Auth />
+        //     </Route>
+        //     {/* <div>
+        //         H!
+        //     </div> */}
+        // </Switch>
     )
 }
 
---
+++ src/Views/ChatModule/ViewAllChats/ViewChatsTable.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    // minWidth: 650,
+    width: '100%',
+    maxWidth: '100%',
+  },
+});
+
+export default function ViewChatsTable(props) {
+  const classes = useStyles();
+  
+  const chats = Object.keys(props.chat).map((key) => 
+
+    <TableRow key={key}>
+        <TableCell>{props.chat[key].from}</TableCell>
+        <TableCell>{props.chat[key].to}</TableCell>
+        <TableCell>
+            {
+                (props.chat[key].priority===1) ? "High" : 
+                (props.chat[key].priority===2) ? "Medium" : "Low"
+            }
+        </TableCell>
+        <TableCell>{props.chat[key].createdAt}</TableCell>
+    </TableRow>
+    )
+
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>From</TableCell>
+            <TableCell>To</TableCell>
+            <TableCell>Priority</TableCell>
+            <TableCell>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {chats}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
