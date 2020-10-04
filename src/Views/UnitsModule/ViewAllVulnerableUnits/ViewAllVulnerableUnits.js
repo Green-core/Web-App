0a05cb45b423366aa0bde9a35af465b4f766c9f0
Binara Medawatta <binaramedawatta@gmail.com>
2020-10-04 20:53:06 +0530

    view vulnerable units

---
+++ src/Views/UnitsModule/ViewAllVulnerableUnits/ViewAllVulnerableUnits.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+import Loading from '../../Loading/Loading';
+import ViewUnitsTable from './ViewUnitsTable';
+import { red } from '@material-ui/core/colors';
+
+export default class ViewAllVulnerableUnits extends React.Component {
+    //constructor
+    constructor(props) {
+        super(props)
+        this.state = {
+            unit: [],
+            loading: false
+        }
+    }
+
+    //component Did mount
+    componentDidMount() {
+        this.setState({ loading: true })
+        console.log("Getting vul devices")
+        axios.get('/units/get-vulnerable-units') // I need the browser 
+            .then((res) => {
+                const unitData = res.data
+                const state = this.state
+                this.setState({
+                    ...state,
+                    unit: { ...unitData },
+                    loading: false
+                })
+            })
+            .catch((error) => console.log(error))
+    }
+
+    //render
+
+    render() {
+        console.log(this.state.loading);
+        const view = this.state.loading ?
+            <Loading /> :
+            <div style={{ padding: 10 }, {backgroundColor:'green' }}>
+                <div style={{fontsize:30},{textAlign: 'center'}}>All Units</div>
+                <ViewUnitsTable unit={this.state.unit} />
+            </div>
+        return (
+            view
+        )
+    }
+}
