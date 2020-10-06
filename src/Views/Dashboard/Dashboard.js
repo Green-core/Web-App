29e46e61b3aa4480305fef4f84a1fc895ae7500b
Binara Medawatta <binaramedawatta@gmail.com>
2020-10-06 10:35:38 +0530

    changed template

--- src/Views/Dashboard/Dashboard.js
+++ src/Views/Dashboard/Dashboard.js
@@ -6,6 +6,14 @@ import { PieChart } from "react-minimal-pie-chart";
 import Card from "react-bootstrap/Card";
 import Map from "./Map/Map";
 // import Map from "./Map/DistrictData";
+// import "../../Template/Template";
+
+import "../../Template/Template.css";
+import Header from "../../Template/Header/Header";
+import Menu from "../../Template/Menu/Menu";
+import Footer from "../../Template/Footer/Footer";
+import Right1 from "../../Template/Right1/Right1";
+import Right2 from "../../Template/Right2/Right2";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -77,70 +85,94 @@ export default function Dashboard() {
   }, []); // <-- Have to pass in [] here!
 
   return (
-    <div className={classes.root} style={{overflow: "hidden"}}>
-      <Grid container spacing={1}>
-        <Grid item xs={6}>
-          <Paper className={classes.paper} style={{ height: "27rem" }}>
-            <Map />
-            {/* <PieChart /> */}
-          </Paper>
-        </Grid>
+    <div className="grid-container">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="left">
+        <Menu />
+      </div>
+      {/* <div className="middle">
+                 <Routes/>
+               </div> */}
+      <div className="right">
+        <Right1 />
+      </div>
+      <div className="right2">
+        <Right2 />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
 
-        <Grid item xs={6}>
-          <Grid container spacing={1}>
-            <Grid item xs={6}>
-              <Card style={{ height: "7rem" }}>
-                <Card.Body>
-                  <Card.Subtitle align="center" className="mb-2 text-muted">
-                    Total Gardners
-                  </Card.Subtitle>
-                  <Card.Title align="center">{userCount}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Grid>
-            <Grid item xs={6}>
-              <Card style={{ height: "7rem" }}>
-                <Card.Body>
-                  <Card.Subtitle align="center" className="mb-2 text-muted">
-                    Total Sensor Modules
-                  </Card.Subtitle>
-                  <Card.Title align="center">{unitCount}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Grid>
-            <Grid item xs={6}>
-              <Card style={{ height: "7rem" }}>
-                <Card.Body>
-                  <Card.Subtitle align="center" className="mb-2 text-muted">
-                    New Tickets
-                  </Card.Subtitle>
-                  <Card.Title align="center">{unreadChatCount}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Grid>
-            <Grid item xs={6}>
-              <Card style={{ height: "7rem" }}>
-                <Card.Body>
-                  <Card.Subtitle align="center" className="mb-2 text-muted">
-                    Vulnerable Units
-                  </Card.Subtitle>
-                  <Card.Title align="center">{vulUnitCount}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Grid>
-            <Grid item xs={6}>
-            <Card style={{ height: "7rem" }}>
-                <Card.Body>
-                  <Card.Subtitle align="center" className="mb-2 text-muted">
-                    Total Plant Types
-                  </Card.Subtitle>
-                  <Card.Title align="center">{plantCount}</Card.Title>
-                </Card.Body>
-              </Card>
+      <div className="middle">
+      <div className={classes.root} style={{ overflow: "hidden" }}>
+        {/* <Template /> */}
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper} style={{ height: "27rem" }}>
+              <Map />
+              {/* <PieChart /> */}
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <Card style={{ height: "7rem" }}>
+                  <Card.Body>
+                    <Card.Subtitle align="center" className="mb-2 text-muted">
+                      Total Gardners
+                    </Card.Subtitle>
+                    <Card.Title align="center">{userCount}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card style={{ height: "7rem" }}>
+                  <Card.Body>
+                    <Card.Subtitle align="center" className="mb-2 text-muted">
+                      Total Sensor Modules
+                    </Card.Subtitle>
+                    <Card.Title align="center">{unitCount}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card style={{ height: "7rem" }}>
+                  <Card.Body>
+                    <Card.Subtitle align="center" className="mb-2 text-muted">
+                      New Tickets
+                    </Card.Subtitle>
+                    <Card.Title align="center">{unreadChatCount}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card style={{ height: "7rem" }}>
+                  <Card.Body>
+                    <Card.Subtitle align="center" className="mb-2 text-muted">
+                      Vulnerable Units
+                    </Card.Subtitle>
+                    <Card.Title align="center">{vulUnitCount}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Card style={{ height: "7rem" }}>
+                  <Card.Body>
+                    <Card.Subtitle align="center" className="mb-2 text-muted">
+                      Total Plant Types
+                    </Card.Subtitle>
+                    <Card.Title align="center">{plantCount}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </div>
+      </div>
     </div>
   );
 }
