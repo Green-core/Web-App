d14227d4b0ece9320f93717bbcdc62f59d700c65
Binara Medawatta <binaramedawatta@gmail.com>
2020-10-06 11:06:13 +0530

    changed template

--- src/Views/ChatModule/UnreadChats/UnreadChats.js
+++ src/Views/ChatModule/UnreadChats/UnreadChats.js
@@ -1,49 +1,73 @@
-import React from 'react'
-import axios from 'axios'
-import Loading from '../../Loading/Loading'
-import UnreadChatsTable from './UnreadChatsTable'
+import React from "react";
+import axios from "axios";
+import Loading from "../../Loading/Loading";
+import UnreadChatsTable from "./UnreadChatsTable";
+
+import "../../../Template/Template.css";
+import Header from "../../../Template/Header/Header";
+import Menu from "../../../Template/Menu/Menu";
+import Footer from "../../../Template/Footer/Footer";
+import Right1 from "../../../Template/Right1/Right1";
+import Right2 from "../../../Template/Right2/Right2";
 
 export default class UnreadChats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      chats: [],
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            loading: false,
-            chats: []
-        }
-    }
+  componentDidMount() {
+    this.setState({ loading: true });
 
-    componentDidMount() {
-        this.setState({ loading: true })
+    axios
+      .get("/chats/get-all-unread")
+      .then((res) => {
+        const chatData = res.data;
+        this.setState({
+          chats: { ...chatData },
+          loading: false,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
 
-        axios
-            .get(
-                '/chats/get-all-unread'
-            )
-            .then((res) => {
-                const chatData = res.data
-                this.setState({
-                    chats: { ...chatData },
-                    loading: false
-                })
-            })
-            .catch((error) => console.log(error))
-    }
+  render() {
+    const view = this.state.loading ? (
+      <Loading />
+    ) : this.state.chats[0] ? (
+      <div style={({ padding: 10 }, { backgroundColor: "green" })}>
+        <div style={({ fontsize: 30 }, { textAlign: "center" })}>
+          Unread Chats
+        </div>
+        <UnreadChatsTable chat={this.state.chats} />
+      </div>
+    ) : (
+      <h1>No Chats</h1>
+    );
 
+    return (
+      <div className="grid-container">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="left">
+          <Menu />
+        </div>
+        <div className="right">
+          <Right1 />
+        </div>
+        <div className="right2">
+          <Right2 />
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
 
-    render() {
-
-        const view = this.state.loading ?
-            <Loading /> :
-            this.state.chats[0] ?
-            <div style={{ padding: 10 }, {backgroundColor:'green' }}>
-                <div style={{fontsize:30},{textAlign: 'center'}}>Unread Chats</div>
-                <UnreadChatsTable chat={this.state.chats} />
-            </div> :
-            <h1>No Chats</h1>
-        
-        return (
-            view
-        )
-    }
+        <div className="middle">{view}</div>
+      </div>
+    );
+  }
 }
